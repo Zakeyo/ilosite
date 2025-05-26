f13f6f0effcb3f12ad8ffa61ef6d3196a1bb6d79
Zakeyo <manuelc.dev@gmail.com>
2025-05-26 11:58:39 -0400

    Reny, solo haz el welcome pls

--- app/Http/Controllers/Sys/UserController.php
+++ app/Http/Controllers/Sys/UserController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Sys;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+ 
 class UserController extends Controller
 {
     public function index()
     {
-        
+
     }
 }
